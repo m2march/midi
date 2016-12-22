485af6d5a7fbefe70795e82bdfd443c0d4044a8a
March <m2.march@gmail.com>
2016-12-22 19:35:05 -0300

    Making tht inner modules generic using the m2 namespace

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='midi',
+      version='0.1',
+      description='Auxiliary midi parsing functions',
+      author='Martin "March" Miguel',
+      author_email='m2.march@gmail.com',
+      packages=['m2.midi'],
+      namespace_packages=['m2'],
+      install_requires=[
+          'python-midi'
+      ]
+      )
